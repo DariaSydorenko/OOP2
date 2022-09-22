36c3639860f457f73a51c877bd1a0b375f5021a0
DariaSydorenko <104034908+DariaSydorenko@users.noreply.github.com>
2022-09-22 23:06:28 +0300

    Update task1-2.py

--- task1-2.py
+++ task1-2.py
@@ -1,16 +1,15 @@
 import argparse
+import math
+import operator
 parser = argparse.ArgumentParser(description='expression')
 parser.add_argument('operation', type=str, help='operation for x1, x2')
 parser.add_argument('x1', type=float, help='the first value')
 parser.add_argument('x2', type=float, help='the second value')
 args = parser.parse_args()
-if args.operation == 'add':
-    print(args.x1+args.x2)
-elif args.operation == 'subtract':
-    print(args.x1-args.x2)
-elif args.operation == 'multiply':
-    print(args.x1*args.x2)
-elif args.operation == 'divide':
-    print(args.x1/args.x2)
-else:
-    print('Error')
+
+result = getattr(math, args.operation, False)
+
+if not result:
+    result = getattr(operator, args.operation)
+
+print(result(args.x1, args.x2))
