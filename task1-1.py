341e756b65fa333f3d68827291dbce4cdd984783
DariaSydorenko <104034908+DariaSydorenko@users.noreply.github.com>
2022-09-22 15:46:04 +0300

    Update task1-1.py

--- task1-1.py
+++ task1-1.py
@@ -2,15 +2,35 @@ import argparse
 parser = argparse.ArgumentParser(description='expression')
 parser.add_argument('x1', type=float, help='the first value')
 parser.add_argument('operation', type=str, help='operation for x1, x2')
-parser.add_argument('x2', type=int, help='the second value')
+parser.add_argument('x2', type=float, help='the second value')
 args = parser.parse_args()
-if args.operation == '+':
-    print(args.x1+args.x2)
-elif args.operation == '-':
-    print(args.x1-args.x2)
-elif args.operation == '*':
-    print(args.x1*args.x2)
-elif args.operation == '/':
-    print(args.x1/args.x2)
+
+
+def addition(x1, x2):
+    return x1 + x2
+
+
+def subtraction(x1, x2):
+    return x1 - x2
+
+
+def multiplication(x1, x2):
+    return x1 * x2
+
+
+def division(x1, x2):
+    return x1 / x2
+
+
+calculator = {
+    '+': addition,
+    '-': subtraction,
+    '*': multiplication,
+    '/': division,
+}
+
+result = calculator.get(args.operation)
+if result:
+    print(result(args.x1, args.x2))
 else:
     print('Error')
