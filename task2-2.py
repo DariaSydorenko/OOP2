2dfd33427de312cda993d46e5e25be65d93d30d4
DariaSydorenko <104034908+DariaSydorenko@users.noreply.github.com>
2022-10-11 20:57:30 +0300

    Update task2-2.py

--- task2-2.py
+++ task2-2.py
@@ -1,30 +1,37 @@
-import argparse
 import math
-parser = argparse.ArgumentParser()
-parser.add_argument('-a', type=int)
-parser.add_argument('-b', type=int)
-args = parser.parse_args()
 
 
 class Rational:
     def __init__(self, numerator, denominator):
-        if numerator is None:
-            numerator = 1
-        if denominator is None:
-            denominator = 1
-        if not (isinstance(numerator, int) or isinstance(denominator, int)):
+        self.gcd = math.gcd(numerator, denominator)
+        self.numerator = numerator
+        self.denominator = denominator
+
+    @property
+    def numerator(self):
+        return self.__numerator
+
+    @property
+    def denominator(self):
+        return self.__denominator
+
+    @numerator.setter
+    def numerator(self, n):
+        if not n:
+            n = 1
+        if not isinstance(n, int):
             raise TypeError
-        if denominator == 0:
+        self.__numerator = n / self.gcd
+
+    @denominator.setter
+    def denominator(self, d):
+        if d == 0:
             raise ZeroDivisionError
-        gcd = math.gcd(numerator, denominator)
-        self.numerator = numerator / gcd
-        self.denominator = denominator / gcd
-
-    def set_numerator(self, n):
-        self.numerator = n
-
-    def set_denominator(self, d):
-        self.denominator = d
+        if not d:
+            d = 1
+        if not isinstance(d, int):
+            raise TypeError
+        self.__denominator = d / self.gcd
 
     def print_rational(self):
         return str(f"{int(self.numerator)}/{int(self.denominator)}")
@@ -33,8 +40,6 @@ class Rational:
         return self.numerator/self.denominator
 
 
-result = Rational(args.a, args.b)
+result = Rational(3, 6)
 print(result.print_rational())
 print(result.print_float())
-
-
